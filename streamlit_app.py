7eefc2ac078f153a12b104cb793f153d91034345
kayla-99 <kayla.wolanski@outlook.com>
2024-06-21 12:02:29 -0400

    Update streamlit_app.py
    fixed syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # convert the Snowpark Dataframe to Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.daraframe(pd_df)
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
