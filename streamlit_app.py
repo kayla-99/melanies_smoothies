92c9b44a620d8cde96200c8db4bbb4fbac9f18fa
kayla-99 <kayla.wolanski@outlook.com>
2024-06-21 11:35:23 -0400

    Update streamlit_app.py
    added code to expose the json data inside the response object

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredients_list:
 ## New section to display fruityvice nutrition info using REST API calls
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
